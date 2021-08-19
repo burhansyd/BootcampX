dc24201ff55963473d6a1b30b27dff8062d1acd1
Burhan Syed <burhan.syed@outlook.com>
2021-08-19 05:04:14 +0000

    refactor to prevent SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const values = [process.argv[2]]
+
+const queryString = `
 SELECT teachers.name, cohorts.name
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = ${process.argv[2]}
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`).then(res => {
+`
+
+pool.query(queryString, values).then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohorts.name}: ${row.teachers.name}`);
   })
